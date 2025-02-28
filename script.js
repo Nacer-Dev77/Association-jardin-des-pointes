5be62ffe0257ad97342ceb3d083151ec2b4bf3d1
NACER <rachedi.nacereddine@gmail.com>
2025-03-01 00:57:29 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let btnBar = document.querySelector('.fa-solid');
+let navBar = document.querySelector('.nav_bar');
+
+
+
+btnBar.addEventListener("click",()=>{
+
+    navBar.classList.toggle("navBar");
+         
+    navBar.classList.toggle("active");
+}) 
+
+ 
+   
+   
+     
+
+
