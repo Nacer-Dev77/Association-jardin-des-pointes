a171eaa2fdfc7792bf26d187f14355479c418c7b
NACER <rachedi.nacereddine@gmail.com>
2025-03-12 02:11:08 +0100

    modification

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-let btnBar = document.querySelector('.fa-solid');
-let navBar = document.querySelector('.nav_bar');
-
-
-
-btnBar.addEventListener("click",()=>{
-
-    navBar.classList.toggle("navBar");
-         
-    navBar.classList.toggle("active");
-}) 
+function toggleMenu() {
+    document.querySelector('.menu-links').classList.toggle('active');
+}
 
  
    
