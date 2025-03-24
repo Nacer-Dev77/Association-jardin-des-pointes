dfb3e14231fcf2030838aaefc4746e2ad7921565
NACER <rachedi.nacereddine@gmail.com>
2025-03-24 18:58:59 +0100

    modification

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ function toggleMenu() {
     document.querySelector('.menu-links').classList.toggle('active');
 }
 
- 
+const yearsSince2012 = new Date().getFullYear() - 2012;
+document.getElementById("years").textContent = yearsSince2012;
+
    
    
      
